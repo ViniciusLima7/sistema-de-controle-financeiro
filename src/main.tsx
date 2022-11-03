0d405d5aba10ac1c4ace3aa55a334d05018bd3e2
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-11-02 22:34:35 -0300

    refactor: Adiciona Arquivo de Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Cadastro from "./pages/Cadastro";
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
-import { Login } from "./pages/Login";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Login />,
-      },
-      {
-        path: "/inicio",
-        element: <Home />,
-      },
-      {
-        path: "/cadastro",
-        element: <Cadastro />,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
---
+++ src/routes/router.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
+import Cadastro from "../pages/Cadastro";
+import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import { Login } from "../pages/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "/inicio",
+        element: <Home />,
+      },
+      {
+        path: "/cadastro",
+        element: <Cadastro />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
