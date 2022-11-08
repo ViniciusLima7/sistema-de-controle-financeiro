60f2a4d27cc658614e063b913394332ebc33c1e7
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-11-08 16:09:53 -0300

    feat: Adiciona Links de Navegação nos Menus

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
-import React from "react";
 import { Box, Container, Image, Section, Text, Title } from "./styles";
 import money from "../../assets/money.svg";
 import profile from "../../assets/profile.svg";
 import settings from "../../assets/settings.svg";
 import notification from "../../assets/notification.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
+  let activeStyle = {
+    color: "var(--red-500)",
+    textDecoration: "none",
+  };
+
+  let style = {
+    textDecoration: "none",
+    color: "var(--white-100)",
+  };
+
   return (
     <Container>
       <Section>
@@ -13,13 +23,62 @@ export default function Header() {
         <Image src={money}></Image>
       </Section>
       <Section>
-        <Text>HOME</Text>
-        <Text>CADASTRO</Text>
-        <Text>DASHBOARD</Text>
-        <Text>CATEGORIAS</Text>
-        <Text>SUBCATEGORIAS</Text>
-        <Text>ECONOMIAS</Text>
-        <Text>RESPONSÁVEIS</Text>
+        <Text>
+          <NavLink
+            to="../inicio"
+            style={({ isActive }) => (isActive ? activeStyle : style)}
+          >
+            HOME
+          </NavLink>
+        </Text>
+        <Text>
+          <NavLink
+            to="../cadastro"
+            style={({ isActive }) => (isActive ? activeStyle : style)}
+          >
+            CADASTRO
+          </NavLink>
+        </Text>
+        <Text>
+          <NavLink
+            to="../dashboard"
+            style={({ isActive }) => (isActive ? activeStyle : style)}
+          >
+            DASHBOARD
+          </NavLink>
+        </Text>
+        <Text>
+          <NavLink
+            to="../categorias"
+            style={({ isActive }) => (isActive ? activeStyle : style)}
+          >
+            CATEGORIAS
+          </NavLink>
+        </Text>
+        <Text>
+          <NavLink
+            to="../subcategorias"
+            style={({ isActive }) => (isActive ? activeStyle : style)}
+          >
+            SUBCATEGORIAS
+          </NavLink>
+        </Text>
+        <Text>
+          <NavLink
+            to="../economias"
+            style={({ isActive }) => (isActive ? activeStyle : style)}
+          >
+            ECONOMIAS
+          </NavLink>
+        </Text>
+        <Text>
+          <NavLink
+            to="../responsaveis"
+            style={({ isActive }) => (isActive ? activeStyle : style)}
+          >
+            RESPONSÁVEIS
+          </NavLink>
+        </Text>
       </Section>
       <Box>
         <Image src={profile}></Image>
