332ac08a487feadf7c9ec66bef0aec665aa2e9e2
Vinicius Lima <marcos.vf@samsung.com>
2024-01-17 23:29:07 -0300

    feat: delete transaction for id

--- src/interfaces/ITitle.ts
+++ src/interfaces/ITitle.ts
@@ -2,8 +2,9 @@ import { ICategory } from "./ICategory";
 import { IEconomy } from "./IEconomy";
 import { IResponsible } from "./IResponsible";
 import { ISubCategory } from "./ISubCategory";
+import { ITransaction } from "./ITransaction";
 
 export interface ITitle {
   title?: string;
-  data?: ICategory | ISubCategory | IResponsible | IEconomy;
+  data?: ICategory | ISubCategory | IResponsible | IEconomy | ITransaction;
 }
