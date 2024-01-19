20d4adfed42714164e7045e5c58b5cf9b27b66d9
Vinicius Lima <marcos.vf@samsung.com>
2024-01-18 22:26:42 -0300

    fix: orderby  transacations

--- src/services/db/firestore/transactions/getTransaction.ts
+++ src/services/db/firestore/transactions/getTransaction.ts
@@ -3,9 +3,14 @@ import { db } from "../../../firebase-config";
 
 export const transactionsCollectionRef = collection(db, "transactions");
 
-export const getTransaction = async (setTransactions: React.Dispatch<any>) => {
+export const getTransaction = async (
+  setTransactions: React.Dispatch<any>,
+  orderByField: string = "date"
+) => {
   try {
-    const data = await getDocs(query(transactionsCollectionRef));
+    const data = await getDocs(
+      query(transactionsCollectionRef, orderBy(orderByField))
+    );
     setTransactions(
       data.docs.map((document) => ({ ...document.data(), id: document.id }))
     );
